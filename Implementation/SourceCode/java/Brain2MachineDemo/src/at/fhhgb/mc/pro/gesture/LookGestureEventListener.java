d53e099ba0102fbf2f14d728f8154190022ec0a5
paulisch <paul.schmutz96@gmail.com>
2018-07-03 14:34:33 +0200

    Installation

--- Implementation/SourceCode/java/Brain2MachineDemo/src/at/fhhgb/mc/pro/gesture/LookGestureEventListener.java
+++ Implementation/SourceCode/java/Brain2MachineDemo/src/at/fhhgb/mc/pro/gesture/LookGestureEventListener.java
@@ -5,5 +5,9 @@ package at.fhhgb.mc.pro.gesture;
  * @author Boris Fuchs, Paul Schmutz
  */
 public interface LookGestureEventListener extends GestureEventListener {
+	/**
+	 * Method that is called when a look is detected.
+	 * @param _evt the event being triggered
+	 */
 	public void onLook(LookGestureEvent _evt);
 }
